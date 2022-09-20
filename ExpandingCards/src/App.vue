fa0c54dc8740bc7e511ec44d419d1e3cbce71139
Devmacy <devmaccyy@gmail.com>
2022-09-20 22:16:18 +0800

    feat: 新增扩展卡片

--- ExpandingCards/src/App.vue
+++ ExpandingCards/src/App.vue
@@ -2,21 +2,31 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import ExpandCards from "./components/ExpandCards.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+<!--  <div>-->
+<!--    <a href="https://vitejs.dev" target="_blank">-->
+<!--      <img src="/vite.svg" class="logo" alt="Vite logo" />-->
+<!--    </a>-->
+<!--    <a href="https://vuejs.org/" target="_blank">-->
+<!--      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />-->
+<!--    </a>-->
+<!--  </div>-->
+<!--  <HelloWorld msg="Vite + Vue" />-->
+  <div class="main-wrap">
+    <ExpandCards></ExpandCards>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
+.main-wrap{
+  background:linear-gradient(45deg,#747bff, #63f253,#747bff, #f674ff);
+  width: 100%;
+  height: 100vh;
+}
+
 .logo {
   height: 6em;
   padding: 1.5em;
---
+++ ExpandingCards/src/components/ExpandCards.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="main-container">
+    <img alt="none photo" :class="photoIndex === index ? 'expand-img-wrap' : 'img-wrap'"
+         v-for="(item,index) in photoList" :key="index" :src="item?.url" @click="setPhotoIndex(index)"
+         class="img-wrap"/>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import {reactive, ref} from "vue";
+
+// 定义数组类型
+interface listType {
+  id: string
+  value?: number | string
+  url?: string
+}
+
+// 创建图片数组
+const photoList: listType[] = reactive([])
+// 创建当前索引
+let photoIndex = ref<number>(0)
+
+// 数组使用reactive赋值
+photoList.push(...[
+    // {id:'',url:''},{id:'',url:''},{id:'',url:''},{id:'',url:''},{id:'',url:''},{id:'',url:''},{id:'',url:''},
+  {
+  id: '1',
+  url: "https://th.bing.com/th/id/R.600958982c0da63858554ae70bcd40cc?rik=cLCY%2bkGidqMqhg&riu=http%3a%2f%2f5b0988e595225.cdn.sohucs.com%2fimages%2f20170911%2f156fce34b4c64235977e354004fa8f28.jpeg&ehk=VGHE7%2f2hbzihYm6b2wdQE9grp5h7%2bA0gWWpixstw7fo%3d&risl=&pid=ImgRaw&r=0"
+}, {
+  id: '1',
+  url: "https://th.bing.com/th/id/R.600958982c0da63858554ae70bcd40cc?rik=cLCY%2bkGidqMqhg&riu=http%3a%2f%2f5b0988e595225.cdn.sohucs.com%2fimages%2f20170911%2f156fce34b4c64235977e354004fa8f28.jpeg&ehk=VGHE7%2f2hbzihYm6b2wdQE9grp5h7%2bA0gWWpixstw7fo%3d&risl=&pid=ImgRaw&r=0"
+}, {
+  id: '2',
+  url: "https://th.bing.com/th/id/R.600958982c0da63858554ae70bcd40cc?rik=cLCY%2bkGidqMqhg&riu=http%3a%2f%2f5b0988e595225.cdn.sohucs.com%2fimages%2f20170911%2f156fce34b4c64235977e354004fa8f28.jpeg&ehk=VGHE7%2f2hbzihYm6b2wdQE9grp5h7%2bA0gWWpixstw7fo%3d&risl=&pid=ImgRaw&r=0"
+}, {
+  id: '2',
+  url: "https://th.bing.com/th/id/R.600958982c0da63858554ae70bcd40cc?rik=cLCY%2bkGidqMqhg&riu=http%3a%2f%2f5b0988e595225.cdn.sohucs.com%2fimages%2f20170911%2f156fce34b4c64235977e354004fa8f28.jpeg&ehk=VGHE7%2f2hbzihYm6b2wdQE9grp5h7%2bA0gWWpixstw7fo%3d&risl=&pid=ImgRaw&r=0"
+}, {
+  id: '2',
+  url: "https://th.bing.com/th/id/R.600958982c0da63858554ae70bcd40cc?rik=cLCY%2bkGidqMqhg&riu=http%3a%2f%2f5b0988e595225.cdn.sohucs.com%2fimages%2f20170911%2f156fce34b4c64235977e354004fa8f28.jpeg&ehk=VGHE7%2f2hbzihYm6b2wdQE9grp5h7%2bA0gWWpixstw7fo%3d&risl=&pid=ImgRaw&r=0"
+}, {
+  id: '2',
+  url: "https://th.bing.com/th/id/R.600958982c0da63858554ae70bcd40cc?rik=cLCY%2bkGidqMqhg&riu=http%3a%2f%2f5b0988e595225.cdn.sohucs.com%2fimages%2f20170911%2f156fce34b4c64235977e354004fa8f28.jpeg&ehk=VGHE7%2f2hbzihYm6b2wdQE9grp5h7%2bA0gWWpixstw7fo%3d&risl=&pid=ImgRaw&r=0"
+}, {
+  id: '2',
+  url: "https://th.bing.com/th/id/R.600958982c0da63858554ae70bcd40cc?rik=cLCY%2bkGidqMqhg&riu=http%3a%2f%2f5b0988e595225.cdn.sohucs.com%2fimages%2f20170911%2f156fce34b4c64235977e354004fa8f28.jpeg&ehk=VGHE7%2f2hbzihYm6b2wdQE9grp5h7%2bA0gWWpixstw7fo%3d&risl=&pid=ImgRaw&r=0"
+}])
+
+
+/**
+ * 设置点击的索引为全局索引
+ * @param clickIndex
+ */
+function setPhotoIndex(clickIndex: number) {
+  photoIndex.value = clickIndex
+}
+
+
+</script>
+
+<style scoped lang="scss">
+.main-container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: auto;
+
+  .img-wrap {
+    max-width: 5%;
+    min-width: 0.1%;
+    height: 90%;
+    border-radius: 2rem;
+    margin: 1%;
+    cursor: pointer;
+    box-shadow: 0 0 0.5rem rgba(248, 248, 248, 0.98);
+    // 贝塞尔曲线
+    transition: all .5s cubic-bezier(0.075, 0.82, 0.165, 1);
+    //  图片自适应
+    object-fit: cover;
+  }
+
+  .expand-img-wrap {
+    @extend .img-wrap;
+    max-width: 30%;
+    min-width: 30%;
+  }
+}
+</style>
