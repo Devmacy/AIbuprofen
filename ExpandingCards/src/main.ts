88ae5262db25cbb74e711c39784c77a67113f5b4
Devmacy <devmaccy@gmail.com>
2022-10-21 17:07:00 +0800

    feat: 新增vue-router

--- ExpandingCards/src/main.ts
+++ ExpandingCards/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './route'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
+app.use(router)
