aabb3ce2ed6100b8246a4e659e7b5de110c3d13b
Devmacy <devmaccy@gmail.com>
2022-11-10 16:15:23 +0800

    feat: 新增十六进制转换十进制方法

--- src/utils/ColorUtils/transformColor.ts
+++ src/utils/ColorUtils/transformColor.ts
@@ -26,16 +26,21 @@ export function getHexOrRgbString(r: number, g: number, b: number, type: string)
  * 十六进制转rgb对象
  * @param hexString 十六进制字符串
  */
-export function getRGBFromHexString(hexString:string = "#fff"):object{
-    const RGBObj =  {
-        r:0,
-        g:0,
-        b:0
+export function getRGBFromHexString(hexString: string = "fff"): object {
+    const length = hexString.length
+    let normalHexString = ''
+    const RGBObj = {
+        r: 0,
+        g: 0,
+        b: 0
     }
-
-    // 如果没有值就返回
-    if(!hexString){
+    // 如果没有值就返回 || 判断输入值的长度
+    if (!hexString || length > 6 || (length !== 3 && length !== 6)) {
         return RGBObj
+    }
+    //输入长度等于3时，转成六位
+    if (length === 3) {
+        normalHexString = getCopyHexString(hexString)
     }
     // 如果不包含#号返回
     const list = hexString.split('#')
@@ -43,8 +48,20 @@ export function getRGBFromHexString(hexString:string = "#fff"):object{
     console.warn(list)
 
     return {
-        r:0,
-        g:0,
-        b:0
+        r: 0,
+        g: 0,
+        b: 0
     }
+}
+
+/**
+ * 将三位十六进制颜色字符串转为10进制
+ * @param params 三维长度十六进制字符串
+ */
+export function getCopyHexString(params: string): string {
+    const hexStringList = params.split('')
+    return hexStringList.reduce((pre, cur) => {
+        pre = pre + cur + cur
+        return pre
+    }, '')
 }
