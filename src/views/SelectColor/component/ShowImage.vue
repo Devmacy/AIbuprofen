2a0e93fc7ff15c66e009db143255f6ccd2a01c5d
Devmacy <devmaccy@gmail.com>
2022-11-11 17:01:45 +0800

    feat: 新增图片选择按钮

--- src/views/SelectColor/component/ShowImage.vue
+++ src/views/SelectColor/component/ShowImage.vue
@@ -1,5 +1,12 @@
 <template>
- 123
+  <el-upload
+      :file-list="fileList"
+      :on-change="handleChange"
+      :show-file-list="false"
+  >
+    <el-button type="primary">select photo</el-button>
+  </el-upload>
+  <img alt="" :src="imgUrl" style="width: 10rem;height: 10rem;">
 </template>
 
 <script lang="ts">
@@ -9,11 +16,28 @@ export default {
 </script>
 
 <script lang="ts" setup>
+import {ref} from "vue";
+import {UploadProps, UploadUserFile} from "element-plus";
+
+let imgUrl = ref<any>('')
+let fileList = ref([] as UploadUserFile[])
+
+const handleChange: UploadProps['onChange'] = (uploadFile) => {
+  debugger
+  fileList.value = []
+  fileList.value.push(uploadFile)
+
+  const a = new FileReader()
+  a.readAsDataURL(uploadFile.raw as File)
+  a.onload = (e:any) => {
+    imgUrl.value = e.target.result
+  }
+}
 
 </script>
 
 <style scoped lang="scss">
-.show-color-cont-main-wrap{
+.show-color-cont-main-wrap {
   height: 100%;
   width: 100%;
 }
