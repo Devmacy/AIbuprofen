f841ad0eebb984ed5ef092033ea6ff6199741a16
Devmacy <devmaccyy@gmail.com>
2022-11-13 23:35:26 +0800

    feat: 新增ctx中获取颜色数组

---
+++ src/utils/ColorUtils/ctxUtils.ts
@@ -0,0 +1,20 @@
+/**
+ * 从ctx图像data中获取到rgb数组
+ * @param ctxImageData ctx的imageData的data
+ */
+export const getRGBList = (ctxImageData:object):Array<number[]> => {
+    // rgb对象数组
+    const rgbList:Array<number> = Object.values(ctxImageData) || []
+    const length = rgbList.length
+
+    // 返回结果
+    const result:Array<number[]> = []
+    for (let i = 0; i < length; i+=4) {
+        const r = rgbList[i]
+        const g = rgbList[i+1]
+        const b = rgbList[i+2]
+        result.push([r,g,b])
+    }
+    console.warn(result)
+    return result
+}
--- src/views/SelectColor/component/ShowImage.vue
+++ src/views/SelectColor/component/ShowImage.vue
@@ -1,12 +1,16 @@
 <template>
-  <el-upload
-      :file-list="fileList"
-      :on-change="handleChange"
-      :show-file-list="false"
-  >
-    <el-button type="primary">select photo</el-button>
-  </el-upload>
-  <img alt="" :src="imgUrl" style="width: 10rem;height: 10rem;">
+  <div class="show-color-cont-main-wrap">
+    <!--    <el-upload-->
+    <!--        :file-list="fileList"-->
+    <!--        :on-change="handleChange"-->
+    <!--        :show-file-list="false"-->
+    <!--    >-->
+    <!--      <el-button type="primary">select photo</el-button>-->
+    <!--    </el-upload>-->
+    <!--    <img alt="" :src="imgUrl" style="max-width: 100%;height: 80%;">-->
+    <el-button @click="getPhoto">1</el-button>
+    <canvas id="photo-canvas" style="max-width: 100%;height: 80%;border: 1px solid #ffff00;box-sizing: border-box"></canvas>
+  </div>
 </template>
 
 <script lang="ts">
@@ -16,23 +20,33 @@ export default {
 </script>
 
 <script lang="ts" setup>
-import {ref} from "vue";
-import {UploadProps, UploadUserFile} from "element-plus";
+import {getRGBList} from "@/utils/ColorUtils/ctxUtils";
 
-let imgUrl = ref<any>('')
-let fileList = ref([] as UploadUserFile[])
-
-const handleChange: UploadProps['onChange'] = (uploadFile) => {
-  debugger
-  fileList.value = []
-  fileList.value.push(uploadFile)
-
-  const a = new FileReader()
-  a.readAsDataURL(uploadFile.raw as File)
-  a.onload = (e:any) => {
-    imgUrl.value = e.target.result
-  }
+const getPhoto = () => {
+  let canvasDom  = document.getElementById('photo-canvas') as HTMLCanvasElement
+  let ctx = canvasDom.getContext('2d') as CanvasRenderingContext2D
+  ctx.fillStyle = "#ff0000"
+  ctx.fillRect (0, 0,  1200, 1200);
+  console.warn(getRGBList(ctx.getImageData(0,0,10,10).data))
 }
+// import {UploadProps, UploadUserFile} from "element-plus";
+//
+// let imgUrl = ref<any>('')
+// let fileList = ref([] as UploadUserFile[])
+//
+// const handleChange: UploadProps['onChange'] = (uploadFile) => {
+//   debugger
+//   fileList.value = []
+//   fileList.value.push(uploadFile)
+//
+//   const a = new FileReader()
+//   a.readAsDataURL(uploadFile.raw as File)
+//   a.onload = (e:any) => {
+//     imgUrl.value = e.target.result
+//   }
+// }
+
+
 
 </script>
 
@@ -40,5 +54,7 @@ const handleChange: UploadProps['onChange'] = (uploadFile) => {
 .show-color-cont-main-wrap {
   height: 100%;
   width: 100%;
+  display: flex;
+  flex-direction: column;
 }
 </style>
